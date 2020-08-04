2f7236e1a1b125e88007927b0a7fc4822b7f48a1
JuanToanXoup <Johndoan42@gmail.com>
2020-08-04 18:45:29 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class PigLatinizer
+    attr_reader :text
+   
+    def initialize
+    end
+
+    def self.piglatinize_word(word)
+        if ["a", "e", "i", "o", "u"].include?(word[0].downcase)
+            pig_array = word + 'way'
+        else
+            const = 0
+            word.split(//).each_with_object([]) do |index,con| 
+                if ["a", "e", "i", "o", "u"].include?(index)
+                    return word.split(//).rotate(const).push('ay').join
+                else
+                    const+=1
+                end
+            end
+        end
+    end
+
+    def piglatinize(words)
+        words.split(/ /).map{|word|PigLatinizer.piglatinize_word(word)}.join(" ")
+    end
+end
